93e5eaa93b1c398111b9929ddac4bbc366454aa8
Doyin Olarewaju <doyinolarewaju@gmail.com>
2020-08-29 03:54:01 +0100

    Create constants within http helper

---
+++ helpers/request.go
@@ -0,0 +1,19 @@
+package helpers
+
+const (
+	MONERIS_PROTOCOL = "https"
+	MONERIS_HOST = "www3.moneris.com"
+	MONERIS_TEST_HOST = "esqa.moneris.com"
+	MONERIS_US_HOST = "esplus.moneris.com"
+	MONERIS_US_TEST_HOST = "esplusqa.moneris.com"
+	MONERIS_PORT = "443"
+	MONERIS_FILE = "/gateway2/servlet/MpgRequest"
+	MONERIS_US_FILE = "/gateway_us/servlet/MpgRequest"
+	MONERIS_MPI_FILE = "/mpi/servlet/MpiServlet"
+	MONERIS_US_MPI_FILE = "/mpi/servlet/MpiServlet"
+	API_VERSION  = ""
+	CONNECT_TIMEOUT = 20
+	CLIENT_TIMEOUT = 35
+)
+
+
