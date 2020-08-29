f177c372809465452e52829ce11c53736750254c
Doyin Olarewaju <doyinolarewaju@gmail.com>
2020-08-29 16:18:36 +0100

    Create package main

--- moneris.go
+++ moneris.go
@@ -1,5 +1,5 @@
-package moneris_go_api
+package main
 
 func main () {
-	// Todo - Get all Interfaces and expose via a single interfacemga
+	// Todo - Get all Interfaces and expose via a single interface
 }
